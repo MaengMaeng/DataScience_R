f13c583c2e15e93fbd6d5b13cf3b219cdc7408a2
MaengMaeng <maybe8189@naver.com>
2018-04-11 18:22:25 +0900

    practice directory

---
+++ practice/practice00.R
@@ -0,0 +1,14 @@
+data("iris")
+newiris<-iris
+newiris$Species <- NULL
+kc <- kmeans(newiris, 3)
+
+str(kc)
+kc$centers
+kc$size
+kc$cluster
+
+table(iris$Species, kc$cluster)
+
+plot(newiris[, c("Sepal.Length", "Sepal.Width")], col = kc$cluster)
+points(kc$centers[,c("Sepal.Length", "Sepal.Width")], col = 1:3, pch = 8, cex=2)
